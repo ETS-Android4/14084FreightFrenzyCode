96dac1cdf6cc7747a7914b67618dbc05f01a1c9a
cprobotics <cprobotics14084@gmail.com>
2021-11-17 11:08:11 -0500

    Test Push
    AB

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap.java
@@ -1,9 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
 
 public class HardwareMap {
     //--Create Motors--//
