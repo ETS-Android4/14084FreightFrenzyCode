6af5d7e9de4daadc82d97f136fc9e13992eebbcc
cprobotics <cprobotics14084@gmail.com>
2021-11-23 15:38:15 -0500

    Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RunToPosition_Test.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.HardwareMap;
+
+@TeleOp
+
+public class RunToPosition_Test extends OpMode {
+    HardwareMap robot = new HardwareMap();
+
+    @Override
+    public void init() {
+        robot.initialize(hardwareMap);
+        robot.clawArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.clawArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.clawArm.setTargetPosition(0);
+        robot.clawArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.clawArm.setPower(.5);
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad2.dpad_up) {
+            robot.clawArm.setTargetPosition(robot.clawArm.getCurrentPosition() +200);
+        } else if (gamepad2.dpad_down) {
+            robot.clawArm.setTargetPosition(robot.clawArm.getCurrentPosition() -200);
+        } else {
+            robot.clawArm.setTargetPosition(robot.clawArm.getCurrentPosition());
+        }
+    }
+}
