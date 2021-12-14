244affdee4ec592811614920f5101c757202bdc9
cprobotics <cprobotics14084@gmail.com>
2021-12-14 15:12:13 -0500

    Disabled TeleOp_Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Test.java
@@ -1,11 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.PIDCoefficients;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name="TeleOp Test", group="robot")
+@Disabled
 
 public class TeleOp_Test extends OpMode {
     HardwareMap robot = new HardwareMap();
