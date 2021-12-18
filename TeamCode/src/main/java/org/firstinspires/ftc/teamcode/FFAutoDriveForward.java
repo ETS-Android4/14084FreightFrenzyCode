ca5b4534c41f1ebb4e0b3e96c92376eb0be466e9
cprobotics <cprobotics14084@gmail.com>
2021-12-18 10:09:47 -0500

    Started Simple Autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FFAutoDriveForward.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name = "FFDriveForward", group = "robot", preselectTeleOp = "FFTeleop1.java")
+
+public class FFAutoDriveForward extends LinearOpMode {
+    HardwareMap robot = new HardwareMap();
+    ElapsedTime timer = new ElapsedTime();
+
+
+    @Override
+    public void runOpMode() {
+        robot.initialize(hardwareMap);
+
+        robot.clawServo.setPosition(0);
+
+        telemetry.addData("Status","Initialized");
+        telemetry.addData("Action", "Press Play to Start");
+        telemetry.update();
+
+        waitForStart();
+
+        timer.reset();
+
+        while (opModeIsActive() && (timer.milliseconds() < 5000)) {
+            telemetry.addData("Timer", timer.milliseconds());
+            telemetry.update();
+        }
+
+        robot.frontRightMotor.setPower(.5);
+        robot.frontLeftMotor.setPower(.5);
+        robot.backRightMotor.setPower(.5);
+        robot.backLeftMotor.setPower(.5);
+
+
+        timer.reset();
+        while (opModeIsActive() && (timer.milliseconds() < 5000)) {
+
+            telemetry.addData("Operation", "Drive Forward");
+            telemetry.update();
+        }
+
+        robot.frontRightMotor.setPower(0);
+        robot.frontLeftMotor.setPower(0);
+        robot.backRightMotor.setPower(0);
+        robot.backLeftMotor.setPower(0);
+
+    }
+}
