a247718e1f66eebf3ad0b6c38b8d4beaf1b1f625
JoxerMoe2 <joxermoe@gmail.com>
2022-01-09 14:05:00 -0500

    added IMU init

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FF_TopBlue_Autonomous1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FF_TopBlue_Autonomous1.java
@@ -2,8 +2,18 @@ package org.firstinspires.ftc.teamcode;
 
 //import statements added automatically
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+import java.util.Locale;
 
 /* Plan
 * Start In front of barcode farthest from spectators blue side
@@ -27,6 +37,11 @@ public class FF_TopBlue_Autonomous1 extends LinearOpMode {
 
     boolean continueLoop = true;
 
+    BNO055IMU imu;
+
+    Orientation angles;
+    Acceleration gravity;
+
     @Override
     //Run Op Mode loop
     public void runOpMode() {
@@ -38,6 +53,20 @@ public class FF_TopBlue_Autonomous1 extends LinearOpMode {
 
 
         //Init IMU
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample OPMode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hardwareMap.get(BNO055IMU.class, "IMU");
+        imu.initialize(parameters);
+
+        composeTelemetry();
+
 
 
         //Add Telemetry text to Driver Station Screen
@@ -64,5 +93,47 @@ public class FF_TopBlue_Autonomous1 extends LinearOpMode {
     }
 
     //Add any other necessary loops here i.e. IMU init, vuforia init, etc.
+
+
+        //IMU init loop
+    void composeTelemetry() {
+
+        // At the beginning of each telemetry update, grab a bunch of data
+        // from the IMU that we will then display in separate lines.
+        telemetry.addAction(() -> {
+            // Acquiring the angles is relatively expensive; we don't want
+            // to do that in each of the three items that need that info, as that's
+            // three times the necessary expense.
+            angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            gravity  = imu.getGravity();
+        });
+
+        telemetry.addLine()
+                .addData("status", () -> imu.getSystemStatus().toShortString())
+                .addData("calibrated", () -> imu.getCalibrationStatus().toString());
+
+        telemetry.addLine()
+                .addData("heading", () -> formatAngle(angles.angleUnit, angles.firstAngle))
+                .addData("roll", () -> formatAngle(angles.angleUnit, angles.secondAngle))
+                .addData("pitch", () -> formatAngle(angles.angleUnit, angles.thirdAngle));
+
+        telemetry.addLine()
+                .addData("gravity", () -> gravity.toString())
+                .addData("mag", () -> String.format(Locale.getDefault(), "%.3f",
+                        Math.sqrt(gravity.xAccel*gravity.xAccel
+                                + gravity.yAccel*gravity.yAccel
+                                + gravity.zAccel*gravity.zAccel)));
+    }
+
+    String formatAngle(AngleUnit angleUnit, double angle) {
+        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+    }
+
+    String formatDegrees(double degrees){
+        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
+    }
+
+
+    //Closing Bracket
 
 }
